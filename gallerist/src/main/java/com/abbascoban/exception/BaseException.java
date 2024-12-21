ba58dfe02b1237098ea872621619dc3deba662ac
Abbas Çoban <abbscbn@gmail.com>
2024-12-21 11:55:07 +0300

    Yorum satırı değişikliği denemesi

--- gallerist/src/main/java/com/abbascoban/exception/BaseException.java
+++ gallerist/src/main/java/com/abbascoban/exception/BaseException.java
@@ -2,6 +2,8 @@ package com.abbascoban.exception;
 
 public class BaseException extends RuntimeException{
 	
+	// Temel hataların fırlatıldığı yer
+	
 	public BaseException(ErrorMessage errorMessage) {
 		super(errorMessage.prepareErrorMessage());
 	}
