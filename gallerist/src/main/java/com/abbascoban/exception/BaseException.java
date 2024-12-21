c1eec1665255861176b59bc8faacb702a5c0659e
Abbas Çoban <abbscbn@gmail.com>
2024-12-21 12:02:33 +0300

    kodların temize çekilmesi

--- gallerist/src/main/java/com/abbascoban/exception/BaseException.java
+++ gallerist/src/main/java/com/abbascoban/exception/BaseException.java
@@ -2,7 +2,7 @@ package com.abbascoban.exception;
 
 public class BaseException extends RuntimeException{
 	
-	// github denemesi
+	
 	
 	public BaseException(ErrorMessage errorMessage) {
 		super(errorMessage.prepareErrorMessage());
