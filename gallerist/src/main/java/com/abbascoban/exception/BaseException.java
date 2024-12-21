db05bfa1e2458964c61a4748c42744b9305f692f
Abbas Çoban <abbscbn@gmail.com>
2024-12-21 11:56:15 +0300

    yorum satırının kaldırılması

--- gallerist/src/main/java/com/abbascoban/exception/BaseException.java
+++ gallerist/src/main/java/com/abbascoban/exception/BaseException.java
@@ -2,7 +2,7 @@ package com.abbascoban.exception;
 
 public class BaseException extends RuntimeException{
 	
-	// Temel hataların fırlatıldığı yer
+	
 	
 	public BaseException(ErrorMessage errorMessage) {
 		super(errorMessage.prepareErrorMessage());
