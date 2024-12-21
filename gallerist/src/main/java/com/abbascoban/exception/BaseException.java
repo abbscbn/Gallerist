09cc9540000ad9da96485ce201bd9239fc2c33de
Abbas Çoban <abbscbn@gmail.com>
2024-12-21 11:59:20 +0300

    github değişikliği denemesi

--- gallerist/src/main/java/com/abbascoban/exception/BaseException.java
+++ gallerist/src/main/java/com/abbascoban/exception/BaseException.java
@@ -2,7 +2,7 @@ package com.abbascoban.exception;
 
 public class BaseException extends RuntimeException{
 	
-	
+	// tekrar
 	
 	public BaseException(ErrorMessage errorMessage) {
 		super(errorMessage.prepareErrorMessage());
