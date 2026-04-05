41ed844c46fe312688d7306e3fe847728f2d3408
abbscbn <abbscbn@gmail.com>
2026-04-05 23:23:13 +0300

    JWT ayarlamaları yapıldı

--- src/main/java/com/abbascoban/gallerist/handler/CustomAccessDeniedHandler.java
+++ src/main/java/com/abbascoban/gallerist/handler/CustomAccessDeniedHandler.java
@@ -1,5 +1,6 @@
 package com.abbascoban.gallerist.handler;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -8,6 +9,7 @@ import org.springframework.security.web.access.AccessDeniedHandler;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
+import java.util.Map;
 
 @Component
 public class CustomAccessDeniedHandler implements AccessDeniedHandler {
@@ -17,14 +19,30 @@ public class CustomAccessDeniedHandler implements AccessDeniedHandler {
                        AccessDeniedException ex) throws IOException {
 
         response.setStatus(HttpServletResponse.SC_FORBIDDEN);
-        response.setContentType("application/json;charset=UTF-8");
-        response.setCharacterEncoding("UTF-8");
 
-        response.getWriter().write("""
-            {
-              "error": "FORBIDDEN",
-              "message": "Bu işlem için yetkiniz yok"
-            }
-        """);
+        response.setContentType("application/json");
+        ObjectMapper mapper = new ObjectMapper();
+
+        String message="Bu işlem için yetkiniz yok";
+
+        Map<String, Object> body = Map.of(
+                "status", 403,
+                "error", "UNAUTHORIZED",
+                "message", message,
+                "path", request.getRequestURI()
+        );
+
+        mapper.writeValue(response.getOutputStream(), body);
+
+
+//        response.setContentType("application/json;charset=UTF-8");
+//        response.setCharacterEncoding("UTF-8");
+//
+//        response.getWriter().write("""
+//            {
+//              "error": "FORBIDDEN",
+//              "message": "Bu işlem için yetkiniz yok"
+//            }
+//        """);
     }
 }
